541d23b77cd3b455a37827f65898cf3c9dfdbf81
rok <post@robert-kummer.de>
2025-05-07 11:49:06 +0200

    Update GitHub repo link in AppSidebar footer navigation
    Replaced the outdated repository link with the correct URL pointing to the updated Laravel starter kit. This ensures users are directed to the proper resource.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -19,7 +19,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/rokde/laravel-starter-kit',
         icon: Folder,
     },
     {
