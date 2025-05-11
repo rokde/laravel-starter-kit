d6c931bd27191cefa78cb8dc2f3d359c21832e2f
rok <post@robert-kummer.de>
2025-05-11 17:16:23 +0200

    Remove ShouldQueue from CreateWorkspaceForRegisteredUser class
    The CreateWorkspaceForRegisteredUser listener no longer implements ShouldQueue. This simplifies its execution flow, implying immediate processing without queuing.

--- app-modules/workspace/src/Listeners/CreateWorkspaceForRegisteredUser.php
+++ app-modules/workspace/src/Listeners/CreateWorkspaceForRegisteredUser.php
@@ -6,10 +6,9 @@ namespace Modules\Workspace\Listeners;
 
 use App\ValueObjects\Id;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\Workspace\Actions\CreateWorkspace;
 
-class CreateWorkspaceForRegisteredUser implements ShouldQueue
+class CreateWorkspaceForRegisteredUser
 {
     public function __construct(
         private readonly CreateWorkspace $createWorkspace,
