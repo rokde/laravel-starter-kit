9237da93aa7eb80180aa5dec2b6ede7f11f0582a
rok <post@robert-kummer.de>
2025-05-11 18:49:46 +0200

    Add route for Workspace index and clean up commented code
    Introduced a new GET route for 'workspaces.index' to display the workspace list. Removed obsolete and unused commented code to improve readability and maintainability of the routes file.

--- app-modules/workspace/routes/web.php
+++ app-modules/workspace/routes/web.php
@@ -9,19 +9,12 @@ Route::middleware(['web', 'auth', 'verified'])
     ->prefix('workspaces')
     ->name('workspaces.')
     ->group(function (): void {
+        Route::get('/', [WorkspaceController::class, 'index'])
+            ->name('index');
+
         Route::post('/', [WorkspaceController::class, 'store'])
             ->name('store');
 
         Route::put('/current', [WorkspaceController::class, 'setCurrent'])
             ->name('set-current');
     });
-
-// use Modules\Workspace\Http\Controllers\WorkspaceController;
-
-// Route::get('/workspaces', [WorkspaceController::class, 'index'])->name('workspaces.index');
-// Route::get('/workspaces/create', [WorkspaceController::class, 'create'])->name('workspaces.create');
-// Route::post('/workspaces', [WorkspaceController::class, 'store'])->name('workspaces.store');
-// Route::get('/workspaces/{workspace}', [WorkspaceController::class, 'show'])->name('workspaces.show');
-// Route::get('/workspaces/{workspace}/edit', [WorkspaceController::class, 'edit'])->name('workspaces.edit');
-// Route::put('/workspaces/{workspace}', [WorkspaceController::class, 'update'])->name('workspaces.update');
-// Route::delete('/workspaces/{workspace}', [WorkspaceController::class, 'destroy'])->name('workspaces.destroy');
