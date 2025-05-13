7ec72dc99b5758a7907113411f96737f62ac898c
rok <post@robert-kummer.de>
2025-05-13 19:18:08 +0200

    Add utility functions for localized number formatting
    Introduce functions for formatting numbers, currency, percentages, and unit-based values using `Intl.NumberFormat`. These utilities handle locale-sensitive formatting and provide fallbacks for unsupported locales.

---
+++ resources/js/lib/number-functions.ts
@@ -0,0 +1,48 @@
+export function localeNumber(number: number | string | null | undefined): string {
+    if (!number) return '';
+
+    const value = Number(number);
+
+    const options: { [key: string]: Intl.NumberFormatOptions } = {};
+
+    options.fallback = { style: 'decimal' };
+
+    return value.toLocaleString(window.locale, options[window.locale] ?? options.fallback);
+}
+
+export function localeMoney(number: number | string | null | undefined, currency: string): string {
+    if (!number) return '';
+
+    const value = Number(number);
+
+    const options: { [key: string]: Intl.NumberFormatOptions } = {};
+
+    options.fallback = { style: 'currency', currencyDisplay: 'symbol', currency };
+
+    return value.toLocaleString(window.locale, options[window.locale] ?? options.fallback);
+}
+
+export function localePercent(number: number | string | null | undefined, fractionDigits: number | undefined = undefined): string {
+    if (!number) return '';
+
+    const value = Number(number);
+
+    const options: { [key: string]: Intl.NumberFormatOptions } = {};
+
+    options.fallback = {
+        style: 'percent',
+        roundingPriority: 'morePrecision',
+        minimumFractionDigits: fractionDigits,
+        minimumSignificantDigits: fractionDigits,
+        minimumIntegerDigits: fractionDigits,
+    };
+
+    return value.toLocaleString(window.locale, options[window.locale] ?? options.fallback);
+}
+
+export function localeNumberUnit(number: number | string | null | undefined, unit: string): string {
+    const value = localeNumber(number);
+    if (!value) return '';
+
+    return `${value} ${unit}}`;
+}
