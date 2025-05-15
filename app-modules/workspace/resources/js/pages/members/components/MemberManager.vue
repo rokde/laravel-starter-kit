d59bbf57f447c986caae1b68848833ec05eabac6
rok <post@robert-kummer.de>
2025-05-15 16:24:12 +0200

    Add optional 'disabled' prop to MemberManager component
    Introduced a new 'disabled' prop with a default value of false to manage the disabled state of the component. Updated the role picker to respect the 'disabled' state, improving flexibility and control in member management scenarios.

--- app-modules/workspace/resources/js/pages/members/components/MemberManager.vue
+++ app-modules/workspace/resources/js/pages/members/components/MemberManager.vue
@@ -9,9 +9,12 @@ import { ref } from 'vue';
 interface Props {
     members: Array<User & { role: string; membership: { role: string } }>;
     roles: { [key: string]: Role };
+    disabled?: boolean;
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+    disabled: false,
+});
 
 const currentId = ref<number | null>(null);
 const form = useForm({
@@ -46,6 +49,7 @@ const modifyRoleForMember = (id: number, role: string) => {
                 :model-value="member.membership.role"
                 @update:model-value="modifyRoleForMember(member.id, $event)"
                 :roles="props.roles"
+                :disabled="props.disabled"
             />
         </div>
     </div>
