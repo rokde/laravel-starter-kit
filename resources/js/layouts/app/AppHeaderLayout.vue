bcdb97c457818112ba5b2ca54ff35e3cd8c62653
rok <post@robert-kummer.de>
2025-05-19 15:21:00 +0200

    Add support for page actions in AppHeader and layouts
    Integrated `pageActions` prop into `AppHeader` and `AppHeaderLayout` components to enable dynamic rendering of page-specific actions. Updated imports and types accordingly to accommodate the new functionality.

--- resources/js/layouts/app/AppHeaderLayout.vue
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -2,20 +2,22 @@
 import AppContent from '@/components/AppContent.vue';
 import AppHeader from '@/components/AppHeader.vue';
 import AppShell from '@/components/AppShell.vue';
-import type { BreadcrumbItemType } from '@/types';
+import type { BreadcrumbItemType, PageActionItemType } from '@/types';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
+    pageActions?: Array<PageActionItemType[]>;
 }
 
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
+    pageActions: () => [],
 });
 </script>
 
 <template>
     <AppShell class="flex-col">
-        <AppHeader :breadcrumbs="breadcrumbs" />
+        <AppHeader :breadcrumbs="breadcrumbs" :page-actions="pageActions" />
         <AppContent>
             <slot />
         </AppContent>
