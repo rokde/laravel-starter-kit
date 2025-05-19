a2e11fc570d38ef277d9d91326951f5f024924f2
rok <post@robert-kummer.de>
2025-05-19 22:35:20 +0200

    Reorder methods and enforce strict typing in interface
    Added `declare(strict_types=1)` to enforce strict typing for better type safety. Reordered methods in the `InAppNotification` interface to improve code readability and maintain consistency.

--- app-modules/notification/src/Contracts/InAppNotification.php
+++ app-modules/notification/src/Contracts/InAppNotification.php
@@ -1,14 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Notification\Contracts;
 
 interface InAppNotification
 {
-    public function getUrl(): ?string;
-
-    public function getTitle(): string;
-
     public static function getDescription(): string;
 
     public static function getGroup(): string;
+
+    public function getUrl(): ?string;
+
+    public function getTitle(): string;
 }
