dc227a63a1d6b7ed1b0d3fdd0427f161cc8f12ef
Robert Kummer <robert@kummer.email>
2025-05-20 13:07:23 +0200

    phpdoc added

--- app-modules/notification/config/notification.php
+++ app-modules/notification/config/notification.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 use Modules\Notification\Notifications\TestNotification;
 
+/**
+ * @return array{
+ *   notifications: class-string<\Modules\Notification\Contracts\InAppNotification>[]
+ * }
+ */
 return [
     'notifications' => [
         TestNotification::class,
--- config/notification.php
+++ config/notification.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 use Modules\Notification\Notifications\TestNotification;
 use Modules\Workspace\Notifications\MemberAcceptedNotification;
 
+/**
+ * @return array{
+ *   notifications: class-string<\Modules\Notification\Contracts\InAppNotification>[]
+ * }
+ */
 return [
     'notifications' => [
         MemberAcceptedNotification::class,
