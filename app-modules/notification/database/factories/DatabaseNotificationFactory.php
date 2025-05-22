6dd196b04bc63df7f14abd2be2a4bf48dcdd4955
rok <post@robert-kummer.de>
2025-05-22 14:25:14 +0200

    Add DatabaseNotificationFactory for notification seeding
    This commit introduces a factory for DatabaseNotification to streamline the seeding of notification data. It includes default state definitions for creating database notifications with relevant fields populated.

---
+++ app-modules/notification/database/factories/DatabaseNotificationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Notification\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Notifications\DatabaseNotification;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Illuminate\Notifications\DatabaseNotification>
+ */
+class DatabaseNotificationFactory extends Factory
+{
+    protected $model = DatabaseNotification::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid(),
+            'type' => 'Modules\Workspace\Notifications\MemberAcceptedNotification',
+            'notifiable_type' => 'App\Models\User',
+            'notifiable_id' => 1,
+            'data' => ['test' => true],
+            'read_at' => null,
+        ];
+    }
+}
