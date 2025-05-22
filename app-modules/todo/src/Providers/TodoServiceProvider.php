ac66cfeb0cae2ab7d594bb7f105705865f42b72e
rok <post@robert-kummer.de>
2025-05-22 17:38:41 +0200

    Remove unused Inertia component registration.
    This removes the unused `registerInertiaComponents` method from the `TodoServiceProvider`. The change simplifies the codebase by excluding unnecessary dependencies and unused logic.

--- app-modules/todo/src/Providers/TodoServiceProvider.php
+++ app-modules/todo/src/Providers/TodoServiceProvider.php
@@ -19,31 +19,5 @@ class TodoServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadTranslationsFrom(__DIR__.'/../../lang', 'todo');
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'todo');
-
-        // Register Inertia components
-//        $this->registerInertiaComponents();
-    }
-
-    /**
-     * Register Inertia components.
-     */
-    private function registerInertiaComponents(): void
-    {
-        $this->app->booted(function (): void {
-            $manager = $this->app->make(\Inertia\ResponseFactory::class);
-
-            $manager->share('todo', fn () => [
-                'routes' => [
-                    'index' => route('todos.index'),
-                    'create' => route('todos.create'),
-                    'store' => route('todos.store'),
-                ],
-            ]);
-
-            $manager->rootView('app');
-
-            $manager->version(fn () => md5_file(public_path('mix-manifest.json') ?? ''));
-        });
     }
 }
