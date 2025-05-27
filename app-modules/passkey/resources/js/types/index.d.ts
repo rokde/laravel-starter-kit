df6c786468213e2dcc67329a2dbc993cf268cad0
Robert Kummer <robert@kummer.email>
2025-05-27 18:25:24 +0200

    add the types

---
+++ app-modules/passkey/resources/js/types/index.d.ts
@@ -0,0 +1,28 @@
+export interface Passkey {
+    id: string;
+    name: string;
+    last_used_at: string | null;
+}
+
+export interface PasskeyOptions {
+    extensions: {
+        extensions: unknown[];
+    };
+    challenge: string;
+    timeout: number|null;
+    rp: {
+        icon: null;
+        id: string;
+        name: string;
+    };
+    user: {
+        displayName: string;
+        icon: null;
+        id: number;
+        name: string;
+    };
+    pubKeyCredParams: unknown[];
+    authenticatorSelection: null;
+    attestation: null;
+    excludeCredentials: null;
+}
