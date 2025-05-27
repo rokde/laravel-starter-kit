c1b09b426af2c5f4dbe9917b7ba5fa7b2d9b2230
Robert Kummer <robert@kummer.email>
2025-05-27 10:06:34 +0200

    added passkeys module with spatie passkey reference

---
+++ app-modules/passkey/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\Passkey\Http\Controllers\PasskeyController;
+
+// Route::get('/passkeys', [PasskeyController::class, 'index'])->name('passkeys.index');
+// Route::get('/passkeys/create', [PasskeyController::class, 'create'])->name('passkeys.create');
+// Route::post('/passkeys', [PasskeyController::class, 'store'])->name('passkeys.store');
+// Route::get('/passkeys/{passkey}', [PasskeyController::class, 'show'])->name('passkeys.show');
+// Route::get('/passkeys/{passkey}/edit', [PasskeyController::class, 'edit'])->name('passkeys.edit');
+// Route::put('/passkeys/{passkey}', [PasskeyController::class, 'update'])->name('passkeys.update');
+// Route::delete('/passkeys/{passkey}', [PasskeyController::class, 'destroy'])->name('passkeys.destroy');
---
+++ app-modules/passkey/src/Providers/PasskeyServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Passkey\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PasskeyServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
