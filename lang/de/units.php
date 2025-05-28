f2bdb74cefa6e82f7030858f7192cdc59cfc88be
Robert Kummer <robert@kummer.email>
2025-05-28 10:39:10 +0200

    added some units for the translation

---
+++ lang/de/units.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'day' => 'Tag',
+    'days' => 'Tage',
+    'hour' => 'Stunde',
+    'hours' => 'Stunden',
+    'minute' => 'Minute',
+    'minutes' => 'Minuten',
+    'month' => 'Monat',
+    'months' => 'Monate',
+    'second' => 'Sekunde',
+    'seconds' => 'Sekunden',
+    'year' => 'Jahr',
+    'years' => 'Jahre',
+];
---
+++ lang/en/units.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'day' => 'day',
+    'days' => 'days',
+    'hour' => 'hour',
+    'hours' => 'hours',
+    'minute' => 'minute',
+    'minutes' => 'minutes',
+    'month' => 'month',
+    'months' => 'months',
+    'second' => 'second',
+    'seconds' => 'seconds',
+    'year' => 'year',
+    'years' => 'years',
+];
