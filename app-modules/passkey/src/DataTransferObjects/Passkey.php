5ab15d9458a9e45bb9f38f27ca94e22271364ea8
Robert Kummer <robert@kummer.email>
2025-05-28 11:06:05 +0200

    added some DTO

---
+++ app-modules/passkey/src/DataTransferObjects/Passkey.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Passkey\DataTransferObjects;
+
+final readonly class Passkey {
+    public function __construct(
+        public int $id,
+        public string $name,
+        public ?string $last_used_at = null,
+    ) {}
+}
