4a0887f6f88e7838d0c9e52fafaa260522f515b6
rok <post@robert-kummer.de>
2025-06-04 11:38:00 +0200

    Remove todo list logic and UI from Index.vue
    This change removes the todo management functionality, including listing, creating, updating, and deleting todos, from the Index.vue file. It simplifies the component and prepares it for a different implementation or restructuring of the todo feature logic.

--- app-modules/todo/resources/js/pages/Index.vue
+++ app-modules/todo/resources/js/pages/Index.vue
@@ -70,14 +70,6 @@ const toggleComplete = (todo: Todo) => {
         },
     );
 };
-
-const deleteTodo = (todo: Todo) => {
-    if (confirm(t('Are you sure you want to delete this todo?'))) {
-        router.delete(route('todos.destroy', todo.id), {
-            preserveScroll: true,
-        });
-    }
-};
 </script>
 
 <template>
@@ -87,35 +79,6 @@ const deleteTodo = (todo: Todo) => {
         <div class="mx-auto my-8 flex w-full max-w-5xl flex-col">
             <div class="mb-6 flex justify-between">
                 <h1 class="text-2xl font-semibold">{{ $t('Todos') }}</h1>
-                <Link :href="route('todos.create')">
-                    <Button>{{ $t('Create Todo') }}</Button>
-                </Link>
-            </div>
-
-            <div v-if="props.data.length === 0" class="rounded-md bg-neutral-50 p-8 text-center">
-                <p class="text-neutral-600">{{ $t('No todos found. Create your first todo!') }}</p>
-            </div>
-
-            <div v-else class="space-y-4">
-                <div v-for="todo in props.data" :key="todo.id" class="flex items-center justify-between rounded-md border border-neutral-200 p-4">
-                    <div class="flex items-center space-x-4">
-                        <Checkbox :checked="todo.completed" @update:modelValue="toggleComplete(todo)" />
-                        <div>
-                            <p :class="{ 'text-neutral-400 line-through': todo.completed }">{{ todo.title }}</p>
-                            <p class="text-sm text-neutral-500" v-if="todo.user">{{ $t('Assigned to') }}: {{ todo.user.name }}</p>
-                        </div>
-                    </div>
-                    <div class="flex space-x-2">
-                        <Link :href="route('todos.update', todo)" method="patch" as="button" :data="{ completed: !todo.completed }">
-                            <Button variant="outline" size="sm">
-                                {{ todo.completed ? $t('Mark as Incomplete') : $t('Mark as Complete') }}
-                            </Button>
-                        </Link>
-                        <Button variant="destructive" size="sm" @click="deleteTodo(todo)">
-                            {{ $t('Delete') }}
-                        </Button>
-                    </div>
-                </div>
             </div>
 
             <DataTable
