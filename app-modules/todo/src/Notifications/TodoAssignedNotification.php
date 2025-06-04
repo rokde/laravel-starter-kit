7d7940fcaf0dc1f979743a719d55ab1cfc60fedd
rok <post@robert-kummer.de>
2025-06-04 15:33:53 +0200

    Add TodoAssignedNotification for task assignment alerts
    Introduces a new notification class to alert users when a task is assigned to them. This ensures users are informed via email and in-app notifications with task details and quick access to their todos.

---
+++ app-modules/todo/src/Notifications/TodoAssignedNotification.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Modules\Todo\Notifications;
+
+use App\DataTransferObjects\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Modules\Notification\Contracts\InAppNotification;
+use Modules\Notification\Notifications\Concerns\SupportUserPreferredChannel;
+use Modules\Todo\Models\Todo;
+
+class TodoAssignedNotification extends Notification implements InAppNotification
+{
+    use Queueable, SupportUserPreferredChannel;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public readonly Todo $todo, public readonly User $user)
+    {
+        //
+    }
+
+    public static function getDescription(): string
+    {
+        return 'Get notified when someone assigns you a todo.';
+    }
+
+    public static function getGroup(): string
+    {
+        return 'Todo';
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('A user assigned you a task')
+            ->line("{$this->user->name} assigned you the task {$this->todo->title}.")
+            ->action('Open your todos', route('todos.index'))
+            ->line('')
+            ->line('You got this email because you have enabled notifications for this type of event.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'todo' => $this->todo,
+            'user' => $this->user,
+        ];
+    }
+
+    public function getUrl(): ?string
+    {
+        return route('workspaces.make-current', ['id' => $this->todo->workspace_id, 'to' => route('todos.index')]);
+    }
+
+    public function getTitle(): string
+    {
+        return __(':user has assigned you the task :task', [
+            'task' => $this->todo->title,
+            'user' => $this->user->name,
+        ]);
+    }
+}
