6865b2140ab86c172b75de57cab571584d2785a6
rok <post@robert-kummer.de>
2025-06-06 14:49:53 +0200

    Add ContentLayout Vue component with layout type prop
    Introduce a new reusable ContentLayout component in Vue. It supports a "type" prop to toggle between "centered" and "full-width" layouts, enabling flexibility in structuring content display.

---
+++ resources/js/layouts/content/ContentLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+    type?: 'centered' | 'full-width';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'centered',
+});
+</script>
+
+<template>
+    <div
+        :class="{
+            'mx-auto my-8 flex w-full max-w-5xl flex-col': props.type === 'centered',
+            'flex h-full flex-1 flex-col gap-4 rounded-xl p-4': props.type === 'full-width',
+        }"
+    >
+        <slot />
+    </div>
+</template>
