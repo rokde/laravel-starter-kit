88827e671bb4ee7a2e34c1fe8264711ab3b4dad6
Robert Kummer <robert@kummer.email>
2025-06-11 08:11:01 +0200

    translation string added

--- resources/js/components/DatePicker.vue
+++ resources/js/components/DatePicker.vue
@@ -2,21 +2,26 @@
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { getI18n } from '@/i18n';
 import { localeDate } from '@/lib/date-functions';
 import { cn } from '@/lib/utils';
 import { CalendarDate, parseDate } from '@internationalized/date';
 import { CalendarIcon } from 'lucide-vue-next';
 import { computed, ref } from 'vue';
 
+const { t } = getI18n();
+
 interface Props {
     modelValue: string | null;
     min?: string | null;
     max?: string | null;
+    placeholder?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
     min: null,
     max: null,
+    placeholder: t('Pick a date'),
 });
 
 const emits = defineEmits<{
@@ -28,7 +33,7 @@ const value = computed<CalendarDate | undefined>({
     set: (val) => val,
 });
 
-const placeholder = ref();
+const calendarPlaceholder = ref();
 
 const minValue = computed<CalendarDate | undefined>(() => (props.min ? parseDate(props.min) : undefined));
 const maxValue = computed<CalendarDate | undefined>(() => (props.max ? parseDate(props.max) : undefined));
@@ -38,14 +43,14 @@ const maxValue = computed<CalendarDate | undefined>(() => (props.max ? parseDate
     <Popover>
         <PopoverTrigger as-child>
             <Button variant="outline" :class="cn('w-[240px] ps-3 text-start font-normal', !value && 'text-muted-foreground')">
-                <span>{{ value ? localeDate(value) : 'Pick a date' }}</span>
+                <span>{{ value ? localeDate(value) : props.placeholder }}</span>
                 <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
             </Button>
             <input hidden />
         </PopoverTrigger>
         <PopoverContent class="w-auto p-0">
             <Calendar
-                v-model:placeholder="placeholder"
+                v-model:placeholder="calendarPlaceholder"
                 :model-value="value"
                 calendar-label="Date of birth"
                 initial-focus
