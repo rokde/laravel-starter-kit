7359922c8341597df50f05ce26a89af14c935d1d
Robert Kummer <robert@kummer.email>
2025-06-11 10:47:41 +0200

    optimized pint and remove final for overridable method

--- app/Data/Facets/AbstractFacet.php
+++ app/Data/Facets/AbstractFacet.php
@@ -64,7 +64,7 @@ abstract class AbstractFacet implements Arrayable, JsonSerializable
         return $this->key;
     }
 
-    final public function assignConditionToQuery(Builder $query, array $values): void
+    public function assignConditionToQuery(Builder $query, array $values): void
     {
         $relation = null;
         $column = $this->column;
