7fc78686fe891fc17f9d9c21898973bbfbbc4508
Robert Kummer <robert@kummer.email>
2025-06-11 08:06:55 +0200

    DatePicker component

---
+++ resources/js/components/DatePicker.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { localeDate } from '@/lib/date-functions';
+import { cn } from '@/lib/utils';
+import { CalendarDate, parseDate } from '@internationalized/date';
+import { CalendarIcon } from 'lucide-vue-next';
+import { computed, ref } from 'vue';
+
+interface Props {
+    modelValue: string | null;
+    min?: string | null;
+    max?: string | null;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    min: null,
+    max: null,
+});
+
+const emits = defineEmits<{
+    (e: 'update:modelValue', date: string | undefined): void;
+}>();
+
+const value = computed<CalendarDate | undefined>({
+    get: () => (props.modelValue ? parseDate(props.modelValue) : undefined),
+    set: (val) => val,
+});
+
+const placeholder = ref();
+
+const minValue = computed<CalendarDate | undefined>(() => (props.min ? parseDate(props.min) : undefined));
+const maxValue = computed<CalendarDate | undefined>(() => (props.max ? parseDate(props.max) : undefined));
+</script>
+
+<template>
+    <Popover>
+        <PopoverTrigger as-child>
+            <Button variant="outline" :class="cn('w-[240px] ps-3 text-start font-normal', !value && 'text-muted-foreground')">
+                <span>{{ value ? localeDate(value) : 'Pick a date' }}</span>
+                <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
+            </Button>
+            <input hidden />
+        </PopoverTrigger>
+        <PopoverContent class="w-auto p-0">
+            <Calendar
+                v-model:placeholder="placeholder"
+                :model-value="value"
+                calendar-label="Date of birth"
+                initial-focus
+                :min-value="minValue"
+                :max-value="maxValue"
+                @update:model-value="
+                    (v) => {
+                        if (v) {
+                            emits('update:modelValue', v.toString());
+                        } else {
+                            emits('update:modelValue', undefined);
+                        }
+                    }
+                "
+            />
+        </PopoverContent>
+    </Popover>
+</template>
