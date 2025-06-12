d9bbe5fe845a5ffa52ddbf9f322c188e3f7fe508
Robert Kummer <robert@kummer.email>
2025-06-12 09:56:44 +0200

    translations added

---
+++ lang/de/timezones.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Africa' => 'Afrika',
+    'Americas' => 'Nord- und Südamerika',
+    'Antartica' => 'Antarktis',
+    'Arctic' => 'Arktis',
+    'Asia' => 'Asien',
+    'Atlantic' => 'Atlantischer Ozean',
+    'Australia' => 'Australien',
+    'Europe' => 'Europa',
+    'Indian Ocean' => 'Indischer Ozean',
+    'Pacific' => 'Pazifischer Ozean',
+];
