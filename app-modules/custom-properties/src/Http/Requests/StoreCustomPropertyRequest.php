b3cdb490d6453db4dfacd5fdf8c7a539ef4c1c42
Robert Kummer <robert@kummer.email>
2025-06-18 22:21:53 +0200

    remove unused `authorize` method from ModifyCustomPropertyRequest

--- app-modules/custom-properties/src/Http/Requests/StoreCustomPropertyRequest.php
+++ app-modules/custom-properties/src/Http/Requests/StoreCustomPropertyRequest.php
@@ -13,14 +13,6 @@ use Modules\CustomProperties\Models\CustomPropertyType;
 class ModifyCustomPropertyRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
