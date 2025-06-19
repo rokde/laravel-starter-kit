6be2641c6f8b6f7a80ac165f2c3b96cf8289381f
Robert Kummer <robert@kummer.email>
2025-06-19 17:36:56 +0200

    add `Todos.vue` page for managing workspace todo settings with breadcrumbs and custom properties manager

---
+++ app-modules/workspace/resources/js/pages/Todos.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { getI18n } from '@/i18n';
+import AppLayout from '@/layouts/AppLayout.vue';
+import type { BreadcrumbItem } from '@/types';
+import CustomPropertiesManager from '@customProperties/components/CustomPropertiesManager.vue';
+import { Head } from '@inertiajs/vue3';
+import WorkspaceSettingsLayout from '@workspace/layouts/WorkspaceSettingsLayout.vue';
+import { Workspace } from '@workspace/types';
+
+const { t } = getI18n();
+
+interface Props {
+    workspace: Workspace;
+}
+
+const props = defineProps<Props>();
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: t('Workspace settings'),
+        href: '/workspaces/current',
+    },
+    {
+        title: props.workspace.name,
+        href: '/workspaces/current',
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <Head :title="$t('Todo settings')" />
+
+        <WorkspaceSettingsLayout>
+            <CustomPropertiesManager :definable="{ id: props.workspace.id, type: 'Modules\\Workspace\\Models\\Workspace' }" />
+        </WorkspaceSettingsLayout>
+    </AppLayout>
+</template>
