66004458adc4716bed8216e5e2a3c2e546fc577f
Robert Kummer <robert@kummer.email>
2025-06-19 17:36:38 +0200

    add `InputDescription.vue` component to display optional description with styling

---
+++ resources/js/components/InputDescription.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+    description?: string;
+}>();
+</script>
+
+<template>
+    <div v-show="description">
+        <p class="text-muted-foreground text-sm">
+            {{ description }}
+        </p>
+    </div>
+</template>
