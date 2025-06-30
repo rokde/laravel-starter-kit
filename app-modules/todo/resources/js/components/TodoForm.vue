477dfa5fddfec19dcc7597d1cdc4779a707f3be9
Robert Kummer <robert@kummer.email>
2025-06-30 21:24:07 +0200

    remove unused i18n import from TodoForm.vue

--- app-modules/todo/resources/js/components/TodoForm.vue
+++ app-modules/todo/resources/js/components/TodoForm.vue
@@ -5,10 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { getI18n } from '@/i18n';
 import { useForm } from '@inertiajs/vue3';
-
-const { t } = getI18n();
 
 interface User {
     id: number;
