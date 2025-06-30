98af0c2e71dee9651df9a14a88c753b1b619f90a
Robert Kummer <robert@kummer.email>
2025-06-30 16:04:46 +0200

    add web routes for role management in the authorization module

---
+++ app-modules/authorization/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Modules\Authorization\Http\Controllers\RolesController;
+
+Route::middleware(['web', 'auth', 'verified'])
+    ->prefix('workspaces/current/roles')
+    ->name('workspaces.roles.')
+    ->group(function (): void {
+        Route::get('/', [RolesController::class, 'index'])
+            ->name('index');
+
+        Route::get('/create', [RolesController::class, 'create'])
+            ->name('create');
+
+        Route::post('/', [RolesController::class, 'store'])
+            ->name('store');
+
+        Route::get('/{role}', [RolesController::class, 'edit'])
+            ->whereNumber('role')
+            ->name('edit');
+
+        Route::put('/{role}', [RolesController::class, 'update'])
+            ->whereNumber('role')
+            ->name('update');
+
+        Route::delete('/{role}', [RolesController::class, 'destroy'])
+            ->whereNumber('role')
+            ->name('destroy');
+    });
