c4d448bd61bd5474b6d0f6263091cb125ca04779
Robert Kummer <robert@kummer.email>
2025-06-30 14:04:09 +0200

    add PermissionCommitCommand to persist permissions to the database

---
+++ app-modules/authorization/src/Console/Commands/PermissionCommitCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Authorization\Console\Commands;
+
+use Illuminate\Console\Command;
+use Modules\Authorization\Permissions\PermissionRegistry;
+
+class PermissionCommitCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'permission:commit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This persists the permissions to the database.';
+
+    public function handle(): int
+    {
+        app(PermissionRegistry::class)
+            ->commit();
+
+        return self::SUCCESS;
+    }
+}
