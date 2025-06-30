b6acb66e5810107ab5d760e36ae2e45be9020133
Robert Kummer <robert@kummer.email>
2025-06-30 16:55:22 +0200

    update AuthorizationServiceProvider to register SetWorkspaceForPermissions middleware for enhanced workspace handling

--- app-modules/authorization/src/Providers/AuthorizationServiceProvider.php
+++ app-modules/authorization/src/Providers/AuthorizationServiceProvider.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Authorization\Providers;
 
+use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\ServiceProvider;
 use Modules\Authorization\Enums\PermissionActions;
 use Modules\Authorization\Enums\PermissionResources;
+use Modules\Authorization\Http\Middleware\SetWorkspaceForPermissions;
 use Modules\Authorization\Permissions\PermissionRegistry;
 
 class AuthorizationServiceProvider extends ServiceProvider
@@ -24,5 +27,15 @@ class AuthorizationServiceProvider extends ServiceProvider
             ->registerPermission(PermissionResources::ROLE, PermissionActions::VIEW, 'View the roles.')
             ->registerPermission(PermissionResources::ROLE, PermissionActions::EDIT, 'Add or update roles.')
             ->registerPermission(PermissionResources::ROLE, PermissionActions::DELETE, 'Delete roles.');
+
+        /** @var Kernel $kernel */
+        $kernel = app(Kernel::class);
+
+        $kernel->addToMiddlewarePriorityBefore(
+            SubstituteBindings::class,
+            SetWorkspaceForPermissions::class,
+        );
+
+        $kernel->appendMiddlewareToGroup('web', SetWorkspaceForPermissions::class);
     }
 }
