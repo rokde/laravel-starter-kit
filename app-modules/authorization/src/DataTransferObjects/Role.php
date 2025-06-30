cedd4288fb08b081a21586b6955f29f2aadd620a
Robert Kummer <robert@kummer.email>
2025-06-30 16:05:01 +0200

    add Role DataTransferObject to enhance type safety and encapsulate role-related data in the authorization module

---
+++ app-modules/authorization/src/DataTransferObjects/Role.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Authorization\DataTransferObjects;
+
+use Spatie\Permission\Models\Role as RoleModel;
+
+readonly class Role
+{
+    public function __construct(
+        public int    $id,
+        public string $name,
+        public array  $permissions,
+    )
+    {
+    }
+
+    public static function fromModel(RoleModel $role): static
+    {
+        $role->loadMissing('permissions');
+
+        return new static(
+            id: $role->id,
+            name: $role->name,
+            permissions: $role->permissions->pluck('name')->toArray(),
+        );
+    }
+}
