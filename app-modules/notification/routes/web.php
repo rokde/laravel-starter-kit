ed95d58f66e6c6f96b52a746c67f886fa3b47983
rok <post@robert-kummer.de>
2025-10-05 11:48:27 +0200

    Add `Route` facade import in notification web routes file

--- app-modules/notification/routes/web.php
+++ app-modules/notification/routes/web.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Modules\Notification\Http\Controllers\NotificationController;
 use Modules\Notification\Http\Controllers\NotificationSettingsController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth', 'verified'])
     ->prefix('notifications')
